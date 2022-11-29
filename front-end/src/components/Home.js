6821d14026b127ae5f8dddda5f728c0fb97ac9d4
Artur <vanivskyi.artur@thinkful.com>
2022-11-28 22:25:53 -0500

    added elements for home page

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -4,11 +4,23 @@ import "./Home.css";
 function Home() {
   return (
     <div className="home">
-      <div className="container">
-        <div className="ring"></div>
-        <div className="ring"></div>
-        <div className="ring"></div>
-        <p>Home info is coming</p>
+      <div className="wrap">
+        <div className="container">
+          <div className="ring"></div>
+          <div className="ring"></div>
+          <div className="ring"></div>
+        </div>
+
+          <div className="bussiness-card">
+            <div className="front">
+              <div className="company">Learn about us!</div>
+              <span className="oi oi-home"></span>
+            </div>
+            <div className="back">
+              <div className="info"></div>
+            </div>
+          </div>
+        
       </div>
     </div>
   );
