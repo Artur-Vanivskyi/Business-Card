895596b006fbc91ca002148b6bc6d7460ea882dd
Artur <vanivskyi.artur@thinkful.com>
2022-12-05 20:22:40 -0500

    chnaged styles, fixed mediaquery

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -1,28 +1,36 @@
 import React from "react";
+import CreateBusinesscard from "./CreateBusinesscard";
 import "./Home.css";
 
 function Home() {
   return (
     <div className="home">
       <div className="wrap">
-        <div className="container">
+        {/* <div className="container">
           <div className="ring"></div>
           <div className="ring"></div>
           <div className="ring"></div>
+        </div> */}
+        <div>
+          <CreateBusinesscard />
         </div>
-
-          <div className="bussiness-card">
-            <div className="front">
-              <div className="company">Learn about us!</div>
-              <span className="oi oi-home"></span>
-            </div>
-            <div className="back">
-              <div className="info">
-                <p>Vancard is the easiest app to manage business cards, the perfect fit for sales people, entrepreneurs, business developers or marketing experts, and anyone who want to be one.</p>
-              </div>
+         <div className="bussiness-card-home">
+          <div className="front-home">
+            <div className="company-home">Learn about us!</div>
+            <span className="oi oi-home"></span>
+          </div>
+          <div className="back-home">
+            <div className="info-home">
+              <p>
+                Vancard is the easiest app to manage business cards, the perfect
+                fit for sales people, entrepreneurs, business developers or
+                marketing experts, and anyone who want to be one.
+              </p>
             </div>
           </div>
-        
+        </div>
+
+
       </div>
     </div>
   );
